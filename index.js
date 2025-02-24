b5766d67966cd7f664458d63efd20fef373e82b3
PRAKASH <prakash@PRAKASHs-MacBook-Pro.local>
2025-02-24 13:21:30 +0530

    Deployment

--- index.js
+++ index.js
@@ -113,5 +113,5 @@ app.post("/new", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on ${port}`);
 });
